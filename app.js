4c5e0469dddbd75e26ba9ef0116db5950dbc50ff
oorochimaru <ryambudha@gmail.com>
2018-12-05 12:54:37 +0530

    final

---
+++ app.js
@@ -0,0 +1,223 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const request = require('request');
+var Mlsschema = require('./model/mls');
+var Today = require('./model/today')
+var Promise = require('bluebird');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/mlsid").then(() => {
+  console.log("db connect")
+}, () => { console.log("err"); });
+
+
+const options = {
+  url: 'https://api.github.com/repos/request/request',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+var token;
+app.get('/go', function (req, res) {
+  request.post('https://stage.retsrabbit.com/api/oauth/access_token', {
+    form:
+    {
+      grant_type: 'client_credentials',
+      client_id: '8zM7Z8xgVqeXgC2APKTSM08NHsgQWHOxmXHXL48q',
+      client_secret: '0fja7hQMrlj8LrzdmeVyCj2IstKXdwTDHdY7Sl4p'
+    }
+  }, function (error, response, body) {
+    token = JSON.parse(body);
+
+    return res.json(token);
+  });
+});
+app.get('/list1950', function (req, res) {
+  request.get({
+    url: 'https://stage.retsrabbit.com/api/v2/property?$filter=year(ModificationTimestamp) gt 2012',
+    headers: {
+      'Authorization': "Bearer " + token.access_token
+    }, json: true
+  }, function (err, response, body) {
+    console.log(token.access_token)
+    property = body;
+    var mls = new Mlsschema({
+      property: property,
+      count: property.value.length
+    });
+    mls.save();
+    console.log(property)
+    return res.json({ property: property, count: property.value.length })
+  })
+});
+
+
+//via insertmany
+app.get('/photosurl', function (req, res) {
+  request.get({
+    url: 'https://stage.retsrabbit.com/api/v2/property?$filter=year(ModificationTimestamp) gt 2012',
+    headers: {
+      'Authorization': "Bearer " + token.access_token
+    }, json: true
+  }, function (err, response, body) {
+      let newBody = body.value.map((value)=>{
+        return {
+          propertyID:value.listing.mls_id,
+          lastModified: value.ModificationTimestamp,
+          ListingContractDate: value.ListingContractDate,
+          imageURI:value.listing.photos.map((photo)=>{
+            return photo.url;
+          }),
+        }
+      });
+      Mlsschema.insertMany(newBody).then(function(results){
+        return res.json({'success':true});
+      }).catch(err=>{
+        return res.json({'success':false});
+
+      })
+      
+  })
+});
+//bluebird promise
+app.get('/photosurl1', function (req, res) {
+  request.get({
+    url: 'https://stage.retsrabbit.com/api/v2/property?$filter=year(ModificationTimestamp) gt 2012',
+    headers: {
+      'Authorization': "Bearer " + token.access_token
+    }, json: true
+  }, function (err, response, body) {
+    console.log(token.access_token)
+    property = body;
+    var mls = new Mlsschema();
+    var createPromise = property.value.map(async value => {
+      mls.propertyID = value.listing.mls_id;
+      mls.count = property.value.length
+      var img = [];
+      await value.listing.photos.map(photos => {
+        img.push(photos.url);
+        // mls.imageURI[value.listing.photos.indexOf(photos)] = photos.url;
+      });
+      return Mlsschema.create(
+        {
+          propertyID: value.listing.mls_id,
+          count: property.value.length,
+          lastModified: value.ModificationTimestamp,
+          imageURI: img,
+          ListingContractDate: value.ListingContractDate
+        })
+    });
+    Promise.all(createPromise).then(function (result) {
+      console.log(result)
+    })
+
+    // mls.save()  
+
+    return res.json({
+      estateID: property.value.id,
+      url: property.value.map(p => p.listing.photos.map(u => { return u.url }))
+    });
+    property.photos.map(p => console.log(p.url));
+    return res.json({ property: property, count: property.value.length })
+  })
+});
+
+//via bluebird Promise
+app.get('/currentdatemodification', function (req, res) {
+  request.get({
+    url: 'https://stage.retsrabbit.com/api/v2/property?$filter=year(ModificationTimestamp) eq 2016 and month(ModificationTimestamp) eq 05 and day(ModificationTimestamp) eq 04',
+    headers: {
+      'Authorization': "Bearer " + token.access_token
+    }, json: true
+  }, function (err, response, body) {
+    var newBody = body.value.map((value)=>{
+      return {
+        propertyID: value.listing.mls_id,
+        lastModified: value.ModificationTimestamp,
+        imageURI: value.listing.photos.map((photo)=>{
+          return photo.url
+        }),
+      }
+    });
+
+    Today.insertMany(newBody).then(function(result){
+      res.json(newBody);
+    }).catch( (err) =>{
+      console.log("error")
+    })
+  })
+});
+
+app.get('/modifiedtoday', function (req, res) {
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
+  console.log(month, day, year)
+  request.get({
+    url: `https://stage.retsrabbit.com/api/v2/property?$filter=year(ModificationTimestamp) eq ${year} and month(ModificationTimestamp) eq ${month} and day(ModificationTimestamp) eq ${day}`,
+    headers: {
+      'Authorization': "Bearer " + token.access_token
+    }, json: true
+  }, function (err, response, body){
+    var newBody = body.value.map((value)=>{
+      return {
+        propertyID: value.listing.mls_id,
+        lastModified: value.ModificationTimestamp,
+        imageURI: value.listing.photos.map((photo)=>{
+          return photo.url
+        }),
+      }
+    });
+
+    Today.insertMany(newBody).then(function(result){
+      res.json(newBody);
+    }).catch( (err) =>{
+      console.log("error")
+    })
+  })
+});
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(3000)
+module.exports = app;
---
+++ model/today.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MlsSchema = new Schema({
+    propertyID: {
+        type: String
+    },
+    count: {
+        type: Number
+    },
+    imageURI: [{
+        type: String
+    }],
+    property: {
+        type: Object
+    },
+    lastModified: {
+        type: Date
+    },
+    ListingContractDate: {
+        type: Date
+    }
+
+});
+
+module.exports = mongoose.model('today', MlsSchema);
